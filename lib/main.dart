0584a9a67f07bc770cedd52b3470918d669fd4fd
alizoort <73581035+alizoort@users.noreply.github.com>
2023-11-09 19:00:25 +0200

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
-import 'package:file_picker/file_picker.dart';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-void main() {
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: true // optional: set false to disable printing logs to console
+  );
+  await Permission.storage.request();
   runApp(const MyApp());
 }
 
@@ -32,114 +39,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  late WebViewController controller;
-  initFilePicker() async {
-    if (Platform.isAndroid) {
-      final androidController = (controller.platform
-      as AndroidWebViewController);
-      await androidController.setOnShowFileSelector(_androidFilePicker);
-    }
-  }
-  Future<List<String>> _androidFilePicker(
-      FileSelectorParams params) async {
-    try {
-      if (params.mode ==
-         FileSelectorMode.openMultiple) {
-        final attachments =
-        await FilePicker.platform.pickFiles(allowMultiple: true);
-        if (attachments == null) return [];
-
-        return attachments.files
-            .where((element) => element.path != null)
-            .map((e) => File(e.path!).uri.toString())
-            .toList();
-      } else {
-        final attachment = await FilePicker.platform.pickFiles();
-        if (attachment == null) return [];
-        File file = File(attachment.files.single.path!);
-        return [file.uri.toString()];
-      }
-    } catch (e) {
-      return [];
-    }
-  }
+  late InAppWebViewController controller;
   @override
   void initState(){
     super.initState();
-    late final PlatformWebViewControllerCreationParams params;
-    if (WebViewPlatform.instance is WebKitWebViewPlatform) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-        mediaTypesRequiringUserAction: const <PlaybackMediaTypes>{},
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    controller = WebViewController.fromPlatformCreationParams(params);
-    controller = WebViewController()..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0x00000000))
-      ..setNavigationDelegate(NavigationDelegate(
-          onProgress: (int progress){
-
-          },
-          onPageStarted:(String url){},
-          onPageFinished: (String url){
-            print("PAGE LOADED");
-          },
-          onWebResourceError: (WebResourceError error){
-            print("INSIDE ERROR");
-            print('${error.description}');
-          },
-          onNavigationRequest: (NavigationRequest request) {
-            return NavigationDecision.navigate;
-          }
-      ))..loadRequest(Uri.parse('http://192.168.120.207:4200')).whenComplete(() => {
-        print("INSIDE LOAD REQUEST ")
-      });
-    if (controller.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (controller.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-      (controller.platform as AndroidWebViewController).setOnShowFileSelector((params)async{
-        print("INSIDE SELECT FILES");
-        return Future.value([]);
-      });
-    }
-    initFilePicker();
   }
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: WebViewWidget(controller: controller),
+        child: InAppWebView(
+          initialUrlRequest: URLRequest.fromMap({"url":"http://192.168.1.107:4200"}),
+         //   initialHeaders: {},
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+                  useOnDownloadStart: true,
+                  allowFileAccessFromFileURLs : true,
+                allowUniversalAccessFromFileURLs: true,
+
+              ),
+            ),
+            onWebViewCreated: (InAppWebViewController controller) {
+              this.controller = controller;
+            //  this.controller.javaScriptHandlersMap
+
+            },
+            onLoadStart: (InAppWebViewController controller, Uri? url) {
+
+            },
+            onLoadStop: (InAppWebViewController controller, Uri? url) {
+
+            },
+            onDownloadStartRequest: (controller, request) async {
+            final Directory directory = await getExternalStorageDirectory() as Directory;
+            final File fileToSave = File('${directory.path}/${request.suggestedFilename as String}');
+            final File pickedFile = File(request.url.path);
+            await fileToSave.writeAsBytes(pickedFile.readAsBytesSync());
+
+            }
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
